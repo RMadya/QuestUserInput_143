bb7a438186c1988490f6d08c0ce34dc6f9841453
RMadya <kereyoben75@gmail.com>
2025-10-31 23:38:56 +0700

    Mendefinisikan wadah Card dengan rounded corner dan shadow (elevation) yang menampung header, form input, dan hasil, memberikan tampilan yang terangkat dan terorganisir.

--- app/src/main/java/com/example/tugaspraktikum/TextEdit.kt
+++ app/src/main/java/com/example/tugaspraktikum/TextEdit.kt
@@ -3,8 +3,13 @@ package com.example.tugaspraktikum
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -54,6 +59,19 @@ fun FormDataDiriStyled(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(vertical = 12.dp)
         ){
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                shape = RoundedCornerShape( size = 18.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .verticalScroll(scrollState) // semua konten di-card bisa discroll
+                ){
 
+                }
         }
     }
